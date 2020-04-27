62a34c2af30c7a57fd8d6da8cd1ed341068cb8b1
ted <teddsbennett@gmail.com>
2020-04-27 10:20:22 +0100

    Initial Commit

---
+++ Fridgy-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fridgy-iOS
+//
+//  Created by Ted Bennett on 27/04/2020.
+//  Copyright © 2020 Ted Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
