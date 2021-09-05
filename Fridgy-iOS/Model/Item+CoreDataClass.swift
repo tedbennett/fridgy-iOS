f0023775e5a48371f59c67b0600d7c1d64319e0a
Ted <teddsbennett@gmail.com>
2021-09-05 13:47:56 +0100

    Update model to single Item, add managed class

---
+++ Fridgy-iOS/Model/Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  Fridgy-iOS
+//
+//  Created by Ted Bennett on 04/09/2021.
+//  Copyright © 2021 Ted Bennett. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+}
---
+++ Fridgy-iOS/Model/Item+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Item+CoreDataProperties.swift
+//  Fridgy-iOS
+//
+//  Created by Ted Bennett on 04/09/2021.
+//  Copyright © 2021 Ted Bennett. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var index: Int16
+    @NSManaged public var inShoppingList: Bool
+    @NSManaged public var name: String
+    @NSManaged public var category: String
+
+}
+
+extension Item : Identifiable {
+
+}
